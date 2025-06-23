0dfc1e801cd99b528846c5c3b20c31c981a75c4a
doptplanning <dopt@doptstudio.com>
2025-06-23 23:42:00 +0900

    fix: CORS 정책을 Vercel 프론트엔드 도메인만 허용하도록 수정

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://my-planner-tool.vercel.app',
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
